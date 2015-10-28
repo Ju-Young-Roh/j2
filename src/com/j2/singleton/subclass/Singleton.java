a224c2029e22e3600a9713d499ef2e73e4b09885
meljay1031 <kevin1031@naver.com>
2015-10-28 13:46:46 +0900

    singleton

---
+++ src/com/j2/singleton/subclass/Singleton.java
@@ -0,0 +1,20 @@
+package com.j2.singleton.subclass;
+
+public class Singleton{
+ 
+  protected static Singleton uniqueInstance;
+  private static int numCalled;
+  
+  protected Singleton(){}
+  
+  public static synchronized Singleton getInstance(){
+    if(uniqueInstance==null){
+      System.out.println("Creating a new instance...");
+      uniqueInstance=new Singleton();
+    }
+    System.out.println("numCalled: "+numCalled++);
+    return uniqueInstance;
+  }
+  
+ 
+}
