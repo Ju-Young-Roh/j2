179841f36838957dbf21d28f09fa60a82815f2a0
Ju-Young-Roh <jyroh13@gmail.com>
2015-11-13 12:08:47 +0900

    enumiter

---
+++ src/com/j2/adapter/enumiter/IteratorEnumeration.java
@@ -0,0 +1,15 @@
+package com.j2.adapter.enumiter
+
+import java.util.*;
+public class IteratorEnumeration implements Enumeration<Object> {
+	Iterator<?> iterator;
+	public IteratorEnumeration(Iterator<?> iterator) {
+		this.iterator = iterator;
+	}
+	public boolean hasMoreElements() {
+		return iterator.hasNext();
+	}
+	public Object nextElement() {
+		return iterator.next();
+	}
+}
